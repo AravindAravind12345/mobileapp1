6aabeaccc25fe4b7d19e20debcf0914bbc5ba7f3
AravindAravind12345 <144437758+AravindAravind12345@users.noreply.github.com>
2025-07-22 10:09:22 +0530

    aravind

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+     
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          
+          title:Text(
+            "SWIGGY", 
+            style:TextStyle(color:Colors.white),
+          
+          
+         
+          )
+         
+        )
+
+      )
+    )
+  );
+}
+
+
